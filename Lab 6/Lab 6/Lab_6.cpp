f26c4737c11782aff62ca00205b17b088020c574
Quan Le <lmmquan@umich.edu>
2024-10-22 13:21:36 -0400

    Add Lab 6 #Add Lab_6.cpp #Add DataPoint class (works like Node) #Add DataSet class (works like LinkedList) #Program now can input the points and output the traverse list like the example of the Lab

---
+++ Lab 6/Lab 6/Lab_6.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+
+using namespace std;
+
+class DataPoint 
+{
+public:
+    float x;
+    float y;
+    DataPoint* next;
+
+    // Constructor
+    DataPoint(float X, float Y) 
+    {
+        x = X;
+        y = Y;
+        next = nullptr;
+    }
+};
+
+class DataSet
+{
+private:
+    DataPoint* head;
+
+public:
+    // Constructor
+    DataSet()
+    {
+        head = nullptr;
+    }
+
+    // Destructor
+    ~DataSet()
+    {
+        DataPoint* cur = head;
+        while (cur != nullptr)
+        {
+            DataPoint* next = cur->next;
+            delete cur;
+            cur = next;
+        }
+    }
+
+    // Insert a node at the beginning of the list:
+    void insertFirstNode(float X, float Y)
+    {
+        DataPoint* newNode = new DataPoint(X,Y);
+        newNode->next = head;
+        head = newNode;
+    }
+
+    // Display the linked list
+    void traverseList() 
+    {
+        cout << "Data Points: " << endl;
+        for (DataPoint* cur = head; cur != nullptr; cur = cur->next) 
+        {
+            cout << "(" << cur->x << ", " << cur->y << ")" << endl;
+        }
+        cout << endl;
+    }
+};
+
+int main()
+{
+    //Declare variable
+    DataSet subset;
+
+    subset.insertFirstNode(4, 7.8);
+    subset.insertFirstNode(3, 5.8);
+    subset.insertFirstNode(2, 3.9);
+    subset.insertFirstNode(1, 2);
+
+    subset.traverseList();
+
+    system("pause");
+    return 0;
+}
